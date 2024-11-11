e5a21f7cfdb736e18843bfb3cdd135eff70d309e
Vijay-Gupta-AI <mevijaygupta2024@gmail.com>
2024-11-10 21:48:48 -0800

    Update utility.py

--- src/utility.py
+++ src/utility.py
@@ -6,7 +6,3 @@ from src.s3 import get_s3_client
 
 s3 = get_s3_client()  # You can use get_s3_client from your s3.py module
 
-# Load environment variables
-dotenv.load_dotenv('system.env')
-Bucket = os.environ.get('BUCKET')
-output_prefix = os.environ.get('OUT_PREFIX')
